0e5b58d98c1ee0d9b2bdc592a01c08e2e62da4bd
taoufik ahjam <taoufikahjam@gmail.com>
2022-04-07 15:50:59 +0200

    add Goals crud

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,50 +1,50 @@
-// import { useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import { useSelector, useDispatch } from 'react-redux'
-// import GoalForm from '../components/GoalForm'
-// import GoalItem from '../components/GoalItem'
-// import Spinner from '../components/Spinner'
-// import { getGoals, reset } from '../features/goals/goalSlice'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
-    //   const navigate = useNavigate()
-    //   const dispatch = useDispatch()
+      const navigate = useNavigate()
+      const dispatch = useDispatch()
     
-    //   const { user } = useSelector((state) => state.auth)
-    //   const { goals, isLoading, isError, message } = useSelector(
-    //     (state) => state.goals
-    //   )
+      const { user } = useSelector((state) => state.auth)
+      const { goals, isLoading, isError, message } = useSelector(
+        (state) => state.goals
+      )
     
-    //   useEffect(() => {
-    //     if (isError) {
-    //       console.log(message)
-    //     }
+      useEffect(() => {
+        if (isError) {
+          console.log(message)
+        }
     
-    //     if (!user) {
-    //       navigate('/login')
-    //     }
+        if (!user) {
+          navigate('/login')
+        }
     
-    //     dispatch(getGoals())
+        dispatch(getGoals())
     
-    //     return () => {
-    //       dispatch(reset())
-    //     }
-    //   }, [user, navigate, isError, message, dispatch])
+        return () => {
+          dispatch(reset())
+        }
+      }, [user, navigate, isError, message, dispatch])
     
-    //   if (isLoading) {
-    //     return <Spinner />
-    //   }
+      if (isLoading) {
+        return <Spinner />
+      }
     
       return (
         <>
           <section className='heading'>
-            {/* <h1>Welcome {user && user.name}</h1> */}
+            <h1>Welcome {user && user.name}</h1>
             <p>Goals Dashboard</p>
           </section>
     
-          {/* <GoalForm /> */}
+          <GoalForm /> 
     
-          {/* <section className='content'>
+          <section className='content'>
             {goals.length > 0 ? (
               <div className='goals'>
                 {goals.map((goal) => (
@@ -54,7 +54,7 @@ function Dashboard() {
             ) : (
               <h3>You have not set any goals</h3>
             )}
-          </section> */}
+          </section>
         </>
       )
     }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
-// import { register, reset } from '../features/auth/authSlice'
-// import Spinner from '../components/Spinner'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -14,26 +14,24 @@ function Register() {
       password2: '',
     })
 
-    const { name, email, password, password2 } = formData
+    const { name, email, password, password2 } = formData 
 
-  //   const navigate = useNavigate()
-  //   const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
-  //   const { user, isLoading, isError, isSuccess, message } = useSelector(
-  //     (state) => state.auth
-  //   )
+    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
 
-  //   useEffect(() => {
-  //     if (isError) {
-  //       toast.error(message)
-  //     }
+    useEffect(() => {
+      if (isError) {
+        toast.error(message)
+      }
 
-  //     if (isSuccess || user) {
-  //       navigate('/')
-  //     }
+      if (isSuccess || user) {
+        navigate('/')
+      }
 
-  //     dispatch(reset())
-  //   }, [user, isError, isSuccess, message, navigate, dispatch])
+      dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
       setFormData((prevState) => ({
@@ -55,13 +53,13 @@ function Register() {
         password,
       };
 
-      // dispatch(register(userData));
+      dispatch(register(userData));
     }
   };
 
-  // if (isLoading) {
-  //   return <Spinner />;
-  // }
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
